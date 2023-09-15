badc146ff9e83ec726c9679d41a972132ad899e6
axon4 <65050809+axon4@users.noreply.github.com>
2023-09-15 23:13:47 +0100

    get base64 from upLoaded PNG from fileReader

--- public/script.js
+++ public/script.js
@@ -1,1 +1,13 @@
-console.log('Image Effects');
+(function () {
+    const inPut = document.getElementsByTagName('input')[0];
+    const fileReader = new FileReader();
+
+    fileReader.onloadend = () => {
+        const base64 = fileReader.result.replace(/^data:image\/png;base64,/, '');
+
+        console.log(inPut.files[0]);
+        console.log(base64);
+    };
+
+    inPut.addEventListener('change', () => {fileReader.readAsDataURL(inPut.files[0])});
+})();
