4efc8ecfd86d2653dbe2e68343b95cc491744aae
axon4 <65050809+axon4@users.noreply.github.com>
2023-09-21 15:12:41 +0100

    load deCoded image from memory

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::console::log_1 as log;
 use base64::decode;
+use image::load_from_memory;
 
 #[wasm_bindgen]
 pub fn greyscale(encoded_image: &str) {
 	log(&"greyScale called".into());
 
 	let base64_to_vector = decode(encoded_image).unwrap();
-
 	log(&"image deCoded".into());
+
+	let image = load_from_memory(&base64_to_vector).unwrap();
+	log(&"image loaded".into());
 }
