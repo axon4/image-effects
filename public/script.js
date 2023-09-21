b09c9baf480145e2195e473ae7037324570b7020
axon4 <65050809+axon4@users.noreply.github.com>
2023-09-21 14:47:01 +0100

    log enCoded string from Rust with Web-Sys--crate

--- public/script.js
+++ public/script.js
@@ -9,16 +9,13 @@
 		return;
 	};
 
-	console.log(rust);
-
 	const inPut = document.getElementsByTagName('input')[0];
 	const fileReader = new FileReader();
 
 	fileReader.onloadend = () => {
 		const base64 = fileReader.result.replace(/^data:image\/png;base64,/, '');
 
-		console.log(inPut.files[0]);
-		console.log(base64);
+		rust.greyscale(base64);
 	};
 
 	inPut.addEventListener('change', () => {fileReader.readAsDataURL(inPut.files[0])});
