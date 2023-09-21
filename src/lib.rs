a6062cda93c9dc1aeda2432652f8def046f3b13c
axon4 <65050809+axon4@users.noreply.github.com>
2023-09-21 15:05:30 +0100

    deCode base64-image inTo u8-vector

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::console::log_1 as log;
+use base64::decode;
 
 #[wasm_bindgen]
-pub fn greyscale(encoded_string: &str) {
-	log(&encoded_string.into());
+pub fn greyscale(encoded_image: &str) {
+	log(&"greyScale called".into());
+
+	let base64_to_vector = decode(encoded_image).unwrap();
+
+	log(&"image deCoded".into());
 }
