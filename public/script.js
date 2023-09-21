8a64a496f95356e3dc42d5465c7c9cd8309b689d
axon4 <65050809+axon4@users.noreply.github.com>
2023-09-21 14:25:25 +0100

    forMat

--- public/script.js
+++ public/script.js
@@ -1,25 +1,25 @@
 (async function () {
-    let rust;
+	let rust;
 
-    try {
-        rust = await import('../pkg');
-    } catch (error) {
-        console.error(error);
+	try {
+		rust = await import('../pkg');
+	} catch (error) {
+		console.error(error);
 
-        return;
-    };
+		return;
+	};
 
-    console.log(rust);
+	console.log(rust);
 
-    const inPut = document.getElementsByTagName('input')[0];
-    const fileReader = new FileReader();
+	const inPut = document.getElementsByTagName('input')[0];
+	const fileReader = new FileReader();
 
-    fileReader.onloadend = () => {
-        const base64 = fileReader.result.replace(/^data:image\/png;base64,/, '');
+	fileReader.onloadend = () => {
+		const base64 = fileReader.result.replace(/^data:image\/png;base64,/, '');
 
-        console.log(inPut.files[0]);
-        console.log(base64);
-    };
+		console.log(inPut.files[0]);
+		console.log(base64);
+	};
 
-    inPut.addEventListener('change', () => {fileReader.readAsDataURL(inPut.files[0])});
+	inPut.addEventListener('change', () => {fileReader.readAsDataURL(inPut.files[0])});
 })();
